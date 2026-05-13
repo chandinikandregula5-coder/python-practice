15b81780c8afa318fc5f7360394b06800a2dfae7
chandini <chandinikandregula5@gmail.com>
2026-05-13 19:56:11 +0530

    DAY 1.py-python practice

---
+++ DAY1.py
@@ -0,0 +1,47 @@
+print("this is my first python class")
+
+"""python is dynamically typed language(we no need to mention data types in python)
+' python is interpreted language (the code runs line by line)
+' python is high level language (we no need worry about low level management and it takes care about it. )
+--------------------------------------------------------------------------------------------------
+'Token(tokens are also known as keywords)
+key words example if,while,for,else,and,or etc
+----------------------------------------------------------------------------------------------------
+Identifiers
+names of variables ,functoins,and classes
+----------------------------------------------------------------------------------------------------
+variables
+which are used to store data.
+the value inside the box is the data.
+the label is the variable name.
+A variable is a named place in memory that stores value program  can use and change.
+--------------------------------------------------------------------------------------------------
+Comments
+In python ,comments are notes written inside bthe code the python ignores when running the program.
+they are used to:
+explain what code does
+make code easier to understand
+temporarily disable code during testing
+
+multi line comments
+python often use triple quotes
+without comments,code can be harder to understand later.
+comments are explanatory notes in python code that are ignored by the interpreter.
+
+------------------------------------------------------------------------------------------------------------
+An object oriented programming language is a language that uses classes and objects to structure programs.
+A procedural programming language is a language where program is written as sequence of steps (procedures)to solve a problem.
+key concept......
+python supports multiple programming styles
+.procedural
+.object oriented programming(oop)
+.functional programming
+"""
+# storing values in a variables 
+name = "chandini"
+age=22
+course = "python full stack"
+#printing the values
+print("Name:",name)
+print("Age:",age)
+print("course",course)
